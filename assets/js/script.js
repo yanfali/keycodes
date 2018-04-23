093af3a66691f81d625b2649c912652f4f5f895b
Yan-Fa Li <yanfali@gmail.com>
2018-04-22 17:54:27 -0700

    p

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,7 @@ $(document).ready(() => {
     var height = 40;
     var xSpace = width * 1.1;
     var ySpace = width * 1.1;
-    var w = _.isUndefined(d.w) ? width : width * d.w;
+    var w = _.isUndefined(d.w) ? width : xSpace * d.w - (xSpace - width);
     var fontSize = '10px';
     var box = el
       .append('rect')
